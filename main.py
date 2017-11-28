a335c904be8f9acf4d69d8953c1e9c54c6e9af0e
Caroline Odden <caroline@codden.no>
2017-11-28 13:37:39 +0100

    fix

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from ner import runNER # Runs NLTK, Stanford, and Polyglot
 from tweepy_collect_data import collect_data # Collect twitterdata, tweepy
 
 
-# Example of list the different methods uses, a double list with [[santized_text, original text, language]]
-texts = [["Daughter of Apple engineer says her father was fired after she posted video of iPhone X", "Daughter of Apple engineer says her father was fired after she posted video of iPhone X!!", "en"]] 
+# Example of list some of the different methods uses, a double list with [[santized_text, original text, language]]
+texts = [["Daughter of Apple engineer says her father was fired after she posted video of iPhone X", "Daughter of Apple engineer says her father was fired after she posted video of iPhone X!!", "en"]]
 
 
 
